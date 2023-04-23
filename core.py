a39588a969a88a295b7a2ad78e82107ceb52b18c
shashvat shah <shashvat911@gmail.com>
2023-04-23 02:54:01 -0400

    requirements and tf to torch core.py

--- core.py
+++ core.py
@@ -1,97 +1,39 @@
 import multiprocessing
 import os
 # import gym
-import tensorflow as tf
-from stable_baselines.a2c.utils import conv, linear, conv_to_fc
-from stable_baselines.common.policies import ActorCriticPolicy, register_policy, nature_cnn
-from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
-from stable_baselines.common.cmd_util import make_atari_env
-from stable_baselines.common.vec_env import VecFrameStack
-from stable_baselines import PPO2, A2C, logger
-from stable_baselines.bench import monitor
+import torch
+import torch.nn as nn
+from stable_baselines3.a2c.utils import conv, linear, conv_to_fc
+from stable_baselines3.common.policies import ActorCriticPolicy, register_policy, nature_cnn
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.cmd_util import make_atari_env
+from stable_baselines3.common.vec_env import VecFrameStack
+from stable_baselines3 import PPO2, A2C, logger
+from stable_baselines3.bench import monitor
 import gfootball.env as football_env
 import numpy as np
 # from keras_layer_normalization import LayerNormalization
-import tensorflow as tf
 import numpy as np
 
 
-def residual_block(inputs, depth):
-	out = tf.keras.layers.Conv2D(depth, 3, 1, padding='same')(inputs)
-	out = tf.keras.layers.ReLU()(out)
-	out = tf.keras.layers.Conv2D(depth, 3, 1, padding='same')(out)
+def residual_block(inputs: torch.Tensor, depth: int) -> torch.Tensor:
+	out = nn.Conv2D(depth, 3, 1, padding='same')(inputs)
+	out = nn.ReLU()(out)
+	out = nn.Conv2D(depth, 3, 1, padding='same')(out)
 	return out + inputs
-
-
-def scaled_dot_product_attention(q, k, v, mask):
-  """Calculate the attention weights.
-  q, k, v must have matching leading dimensions.
-  k, v must have matching penultimate dimension, i.e.: seq_len_k = seq_len_v.
-  The mask has different shapes depending on its type(padding or look ahead)
-  but it must be broadcastable for addition.
-
-  Args:
-	q: query shape == (..., seq_len_q, depth)
-	k: key shape == (..., seq_len_k, depth)
-	v: value shape == (..., seq_len_v, depth_v)
-	mask: Float tensor with shape broadcastable
-		  to (..., seq_len_q, seq_len_k). Defaults to None.
-
-  Returns:
-	output, attention_weights
-  """
-
-  matmul_qk = tf.matmul(q, k, transpose_b=True)  # (..., seq_len_q, seq_len_k)
-  print("q " + str(q.shape))
-  print("k" + str(k.shape))
-  print("qk" + str(matmul_qk.shape))
-  # scale matmul_qk
-  dk = tf.cast(tf.shape(k)[-1], tf.float32)
-  scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
-  print("softmax" + str(scaled_attention_logits.shape))
-  # add the mask to the scaled tensor.
-  # if mask is not None:
-  #   scaled_attention_logits += (mask * -1e9)
-
-  # softmax is normalized on the last axis (seq_len_k) so that the scores
-  # add up to 1.
-
-  attention_weights = tf.nn.softmax(
-      scaled_attention_logits, axis=-1)  # (..., seq_len_q, seq_len_k)
-
-  output = tf.matmul(attention_weights, v)  # (..., seq_len_q, depth_v)
-  print("output" + str(output.shape))
-  return output
-
-# class MultiHeadAttention(tf.keras.layers.Layer):
-#   def __init__(self, d_model, num_heads):
-#     super(MultiHeadAttention, self).__init__()
-#     self.num_heads = num_heads
-#     self.d_model = d_model
-
-#     assert d_model % self.num_heads == 0
-
-#     self.depth = d_model // self.num_heads
-
-#     self.wq = tf.keras.layers.Dense(d_model)
-#     self.wk = tf.keras.layers.Dense(d_model)
-#     self.wv = tf.keras.layers.Dense(d_model)
-
-#     self.dense = tf.keras.layers.Dense(d_model)
-
 
 def split_heads(x, num_heads):
   """Split the last dimension into (num_heads, depth).
   Transpose the result such that the shape is (batch_size, num_heads, seq_len, depth)
   """
-  c = tf.broadcast_to(x, [num_heads, tf.shape(x)[0], x.shape[1], x.shape[2]])
-  return tf.transpose(c, perm=[1, 0, 2, 3])
+  c = torch.broadcast_to(x, [num_heads, torch.shape(x)[0], x.shape[1], x.shape[2]])
+  return torch.transpose(c, perm=[1, 0, 2, 3])
 
 
 def mlp(x):
-	y = tf.keras.layers.Dense(384, activation='relu')(x)
-	y = tf.keras.layers.Dense(384, activation='relu')(y)
-	y = tf.keras.layers.Dense(54, activation='relu')(y)
+	y = nn.Dense(384, activation='relu')(x)
+	y = nn.Dense(384, activation='relu')(y)
+	y = nn.Dense(54, activation='relu')(y)
 	return y
 
 # def layer_norm(x):
@@ -108,9 +50,9 @@ def mhdpa(v, k, q, num_heads):
   # k = self.wk(k)  # (batch_size, seq_len, d_model)
   # v = self.wv(v)  # (batch_size, seq_len, d_model)
 
-  wq = tf.keras.layers.Dense(18)(q)
-  wk = tf.keras.layers.Dense(18)(k)
-  wv = tf.keras.layers.Dense(18)(v)
+  wq = nn.Dense(18)(q)
+  wk = nn.Dense(18)(k)
+  wv = nn.Dense(18)(v)
 #   wq = LayerNormalization()(wq)
 #   wk = LayerNormalization()(wk)
 #   wv = LayerNormalization()(wv)
@@ -121,18 +63,18 @@ def mhdpa(v, k, q, num_heads):
 
   # scaled_attention.shape == (batch_size, num_heads, seq_len_q, depth)
   # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
-  scaled_attention = scaled_dot_product_attention(
-	  wq, wk, wv, num_heads)
+  scaled_attention = nn.functional.scaled_dot_product_attention(
+	  wq, wk, wv)
   print("sca " + str(scaled_attention))
   # (batch_size, seq_len_q, num_heads, depth)
-  scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+  scaled_attention = torch.transpose(scaled_attention, perm=[0, 2, 1, 3])
   print("sca_ " + str(scaled_attention))
-  concat_attention = tf.reshape(scaled_attention,
-                                (tf.shape(scaled_attention)[0], scaled_attention.shape[1], scaled_attention.shape[2]*scaled_attention.shape[3]))  # (batch_size, seq_len_q, d_model)
+  concat_attention = torch.reshape(scaled_attention,
+                                (torch.shape(scaled_attention)[0], scaled_attention.shape[1], scaled_attention.shape[2]*scaled_attention.shape[3]))  # (batch_size, seq_len_q, d_model)
   print("concat" + str(concat_attention))
 #   output = mlp(concat_attention)
 #   output = output + q
-  output = tf.keras.layers.Dense(scaled_attention.shape[2]*scaled_attention.shape[3])(concat_attention)
+  output = nn.Dense(scaled_attention.shape[2]*scaled_attention.shape[3])(concat_attention)
 #   output = LayerNorma1lization()(output)
   return output
 
@@ -173,26 +115,24 @@ def build_impala_cnn(unscaled_images, depths=[16, 32, 32], **conv_kwargs):
 		return num_str
 
 	def conv_layer(out, depth):
-		return tf.layers.conv2d(out, depth, 3, padding='same', name='layer_' + get_layer_num_str())
+		return nn.functinal.conv2d(out, depth, 3, padding='same', name='layer_' + get_layer_num_str())
 
 	def residual_block(inputs):
 		depth = inputs.get_shape()[-1].value
-
-		out = tf.nn.relu(inputs)
-
+		out = nn.relu(inputs)
 		out = conv_layer(out, depth)
-		out = tf.nn.relu(out)
+		out = nn.relu(out)
 		out = conv_layer(out, depth)
 		return out + inputs
 
 	def conv_sequence(inputs, depth):
 		out = conv_layer(inputs, depth)
-		out = tf.layers.max_pooling2d(out, pool_size=3, strides=2, padding='same')
+		out = nn.functional.max_pool2d(out, pool_size=3, strides=2, padding='same')
 		out = residual_block(out)
 		out = residual_block(out)
 		return out
 
-	out = tf.cast(unscaled_images, tf.float32) / 255.
+	out = torch.cast(unscaled_images, torch.float32) / 255.
 	# y = tf.keras.layers.Conv2D(32, 4, 2, padding='same')(out)
 	# y = tf.keras.layers.BatchNormalization()(y)
 	# y = tf.keras.layers.ReLU()(y)
@@ -214,17 +154,14 @@ def build_impala_cnn(unscaled_images, depths=[16, 32, 32], **conv_kwargs):
 		out = conv_sequence(out, depth)
 	# out = tf.keras.layers.ConvLSTM2D(32,2,1,'same1')
 	# out = tf.keras.layers.Conv2D(54,3,1,'same')(out)
-	print(tf.shape(out)[0])
-	print(tf.shape(out)[1])
-	print(tf.shape(out)[2])
-	print(tf.shape(out)[3])
-	out = tf.reshape(
-		out, (tf.shape(out)[0], out.shape[1]*out.shape[2], out.shape[3]))
+	
+	out = torch.reshape(
+		out, (torch.shape(out)[0], out.shape[1]*out.shape[2], out.shape[3]))
 	print(out.shape)
 	out = mhdpa(out, out, out, 3)
-	out = tf.keras.layers.Flatten()(out)
-	out = tf.nn.relu(out)
-	out = tf.layers.dense(out, 256, activation=tf.nn.relu)
+	out = nn.Flatten()(out)
+	out = nn.functional.relu(out)
+	out = nn.functional.dense(out, 256, activation=nn.ReLU)
 
 	return out
 
@@ -237,28 +174,27 @@ class CustomPolicy(ActorCriticPolicy):
 		super(CustomPolicy, self).__init__(sess, ob_space, ac_space,
                                      n_env, n_steps, n_batch, reuse=reuse, scale=True)
 
-		with tf.variable_scope("model", reuse=reuse):
-			activ = tf.nn.relu
-			print("processed obs" + str(self.processed_obs.shape))
-			extracted_features = build_impala_cnn(self.processed_obs, **kwargs)
-			print("extracted_features"+str(extracted_features.shape))
-			extracted_features = tf.layers.flatten(extracted_features)
-			print("ex2 " + str(extracted_features.shape))
+		activation = nn.ReLU()
+		print("processed obs" + str(self.processed_obs.shape))
+		extracted_features = build_impala_cnn(self.processed_obs, **kwargs)
+		print("extracted_features"+str(extracted_features.shape))
+		extracted_features = nn.flatten(extracted_features)
+		print("ex2 " + str(extracted_features.shape))
 
-			pi_h = extracted_features
-			for i, layer_size in enumerate([128, 128, 128]):
-				pi_h = activ(tf.layers.dense(pi_h, layer_size, name='pi_fc' + str(i)))
-			pi_latent = pi_h
+		pi_h = extracted_features
+		for i, layer_size in enumerate([128, 128, 128]):
+			pi_h = activation(nn.Dense(pi_h, layer_size, name='pi_fc' + str(i)))
+		pi_latent = pi_h
 
-			vf_h = extracted_features
-			for i, layer_size in enumerate([32, 32]):
-				vf_h = activ(tf.layers.dense(vf_h, layer_size, name='vf_fc' + str(i)))
-			value_fn = tf.layers.dense(vf_h, 1, name='vf')
-			vf_latent = vf_h
+		vf_h = extracted_features
+		for i, layer_size in enumerate([32, 32]):
+			vf_h = activation(nn.Dense(vf_h, layer_size, name='vf_fc' + str(i)))
+		value_fn = nn.Dense(vf_h, 1, name='vf')
+		vf_latent = vf_h
 
-			self._proba_distribution, self._policy, self.q_value = \
-				self.pdtype.proba_distribution_from_latent(
-					pi_latent, vf_latent, init_scale=0.01)
+		self._proba_distribution, self._policy, self.q_value = \
+			self.pdtype.proba_distribution_from_latent(
+				pi_latent, vf_latent, init_scale=0.01)
 
 		self._value_fn = value_fn
 		self._setup_init()
@@ -281,14 +217,6 @@ class CustomPolicy(ActorCriticPolicy):
 
 def create_single_football_env(seed):
   """Creates gfootball environment."""
-#   env = football_env.create_environment(
-#       env_name=FLAGS.level, stacked=('stacked' in FLAGS.state),
-#       rewards=FLAGS.reward_experiment,
-#       logdir=logger.get_dir() + str(seed),
-#       enable_goal_videos=FLAGS.dump_scores and (seed == 0),
-#       enable_full_episode_videos=FLAGS.dump_full_episodes and (seed == 0),
-#       render= True and (seed == 0),
-#       dump_frequency=50 if FLAGS.render and seed == 0 else 0)
 
   env = football_env.create_environment(env_name="academy_3_vs_1_with_keeper", stacked=True,
                                         representation='extracted', render=False and (seed == 0), channel_dimensions=(64, 64), rewards='scoring,checkpoints')
@@ -303,14 +231,6 @@ def callback(_, _t):
 	print("model saved")
 	return True
 
-ncpu = multiprocessing.cpu_count()
-config = tf.ConfigProto(allow_soft_placement=True,
-                        intra_op_parallelism_threads=ncpu,
-                        inter_op_parallelism_threads=ncpu)
-config.gpu_options.allow_growth = True
-
-tf.Session(config=config).__enter__()
-logger.configure(folder="CNN")
 vec_env = SubprocVecEnv([
 	(lambda _i=i: create_single_football_env(_i))
 	for i in range(8)])
